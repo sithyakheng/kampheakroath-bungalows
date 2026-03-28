e67fb92b0a44941db815a8d87ef5171ab8967fc4
sithyakheng <sithyakheng@gmail.com>
2026-03-28 20:50:15 +0700

    remove node_modules and .next

--- .next/dev/static/chunks/src_app_page_tsx_03cd6-z._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/node_modules_02fw2.7._.js","static/chunks/src_components_bungalow_0yqwu2e._.js"],
-    source: "dynamic"
-});
